8cf1ffe3b116a2bc71e26c24b535dac78cbe1ef5
Lucas <lzalaquett@uc.cl>
2020-09-10 13:05:18 -0300

    todos los csv con python

--- Entrega2/limpiador_buques.py
+++ Entrega2/limpiador_buques.py
@@ -8,18 +8,17 @@ for linea in lista_buques:
     linea = linea.strip('\n')
     campos = linea.split(',')
     nueva_lisat_buques.append(campos)
-'''
-print(len(nueva_lisat_buques[0]))
-print(len(nueva_lisat_buques[1]))
-print(nueva_lisat_buques[0])
-print(nueva_lisat_buques[1])
-'''
 
 with open('solo_buques.csv', 'w') as nbf:
     for instanica in nueva_lisat_buques:
-        for n_atributo in range(3):
-            nbf.write(instanica[n_atributo])
-            nbf.write(',')
+        nbf.write(instanica[0])
+        nbf.write(',')
+        nbf.write(instanica[1])
+        nbf.write(',')
+        nbf.write(instanica[2])
+        nbf.write(',')
+        nbf.write(instanica[3])
+        nbf.write(',')
         nbf.write(instanica[4])
         nbf.write('\n')
 
@@ -77,3 +76,97 @@ with open ('cargueros.csv', 'w') as car:
             car.write(',')
             car.write(instanica[7])
             car.write('\n')
+
+lista_capitanes = []
+
+for instanica in nueva_lisat_buques:
+    lista_capitanes.append(instanica[9])
+
+
+with open('personal_buque.csv', 'r') as pb_file:
+    lista_personal = pb_file.readlines()
+
+nueva_lisat_personal = []
+
+for linea in lista_personal:
+    linea = linea.strip('\n')
+    campos = linea.split(',')
+    nueva_lisat_personal.append(campos)
+
+with open('nuevo_personal.csv', 'w') as np:
+    for instanica in nueva_lisat_personal:
+        np.write(instanica[0])
+        np.write(',')
+        np.write(instanica[1])
+        np.write(',')
+        np.write(instanica[4])
+        np.write(',')
+        if instanica[0] in lista_capitanes:
+            np.write('capitán')
+        else:
+            np.write('marinero')
+        np.write(',')
+        np.write(instanica[5])
+        np.write(',')
+        np.write(instanica[2])
+        np.write(',')
+        np.write(instanica[3])
+        np.write('\n')
+
+with open('tiene.csv', 'w') as tiene:
+    for instanica in nueva_lisat_personal:
+        tiene.write(instanica[6])
+        tiene.write(',')
+        tiene.write(instanica[0])
+        tiene.write('\n')
+
+
+
+with open('itinerarios.csv', 'r') as it_file:
+    lit = it_file.readlines()
+
+nlit = []
+
+for linea in lit:
+    linea = linea.strip('\n')
+    campos = linea.split(',')
+    nlit.append(campos)
+
+
+dict_puertos = {}
+
+contador = -1
+
+with open('puertos.csv', 'w') as pts:
+    for instanica in nlit:
+        if not instanica[3] in dict_puertos.keys():
+            dict_puertos[instanica[3]] = str(contador)
+            pts.write(str(contador))
+            contador += 1
+            pts.write(',')
+            pts.write(instanica[3])
+            pts.write('\n')
+
+
+with open('atraques.csv', 'w') as atr:
+    for instanica in nlit:
+        if instanica[1]:
+            atr.write(instanica[2])
+            atr.write(',')
+            atr.write(dict_puertos[instanica[3]])
+            atr.write(',')
+            atr.write(instanica[0])
+            atr.write(',')
+            atr.write(instanica[1])
+            atr.write('\n')
+
+
+with open('prox_itinerario.csv', 'w') as prx:
+    for instanica in nlit:
+        if not instanica[1] or instanica[1] == 'fecha_salida':
+            prx.write(instanica[2])
+            prx.write(',')
+            prx.write(dict_puertos[instanica[3]])
+            prx.write(',')
+            prx.write(instanica[0])
+            prx.write('\n')
--- Sites/consultas/consulta_tipo_nombre.php
+++ Sites/consultas/consulta_tipo_nombre.php
@@ -9,7 +9,7 @@
 	$tipo = $_POST["tipo_elegido"];
 	$nombre = $_POST["nombre_pokemon"];
 
- 	$query = "SELECT id,nombre, tipo FROM pokemones WHERE tipo LIKE '%$tipo%' AND nombre LIKE '%$nombre%';";
+ 	$query = "SELECT nombre, pais FROM navieras WHERE pais LIKE '%$pais%' AND nombre LIKE '%$nombre%';";
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$pokemones = $result -> fetchAll();
