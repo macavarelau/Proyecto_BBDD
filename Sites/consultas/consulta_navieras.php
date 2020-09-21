be70bbc01f71640138136cd1cbdbc29160cdca89
Lucas <lzalaquett@uc.cl>
2020-09-21 17:13:38 -0300

    intento de poner bootstrap

--- Sites/consultas/consulta_navieras.php
+++ Sites/consultas/consulta_navieras.php
@@ -6,23 +6,19 @@
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
 
-	$país = $_POST["tipo_elegido"];
-	$nombre = $_POST["nombre_naviera"];
-
- 	$query = "SELECT nombre, país FROM navieras WHERE UPPER(país) LIKE UPPER('%$país%') AND nombre LIKE '%$nombre%';";
+ 	$query = "SELECT nombre, país FROM navieras;";
 	$result = $db -> prepare($query);
 	$result -> execute();
 	$navieras = $result -> fetchAll();
   ?>
-
+	<h1>Aquí podrá ver lista con los nombres de todas las navieras:</h1>
 	<table class="table">
     <tr>
       <th>NOMBRE</th>
-      <th>PAÍS</th>
     </tr>
   <?php
 	foreach ($navieras as $naviera) {
-  		echo "<tr> <td>$naviera[0]</td> <td>$naviera[1]</td> </tr>";
+  		echo "<tr> <td>$naviera</td> </tr>";
 	}
   ?>
 	</table>
