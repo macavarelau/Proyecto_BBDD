820526b94a11f5266d0db2cc4c498d0fd28df28d
Lucas <lzalaquett@uc.cl>
2020-09-24 15:44:33 -0300

    poniendo bootstrapppppp

--- Sites/consultas/consulta_5.php
+++ Sites/consultas/consulta_5.php
@@ -14,7 +14,7 @@
   $puerto = $_POST["puerto"];
 
   #Se construye la consulta como un string
-  $query = "SELECT DISTINCT personal.nombre from personal, tiene, buques, atraques, puertos where personal.genero = '$genero' and personal.cargo = '$cargo' and personal.pid = tiene.pid and buques.bid = tiene.bid and atraques.abid = buques.bid and puertos.ptid = atraques.puerto and UPPER(puertos.pnombre) like UPPER('%$puerto%');";
+  $query = "SELECT DISTINCT personal.pid, personal.nombre, personal.edad, personal.cargo, personal.genero, personal.nacionalidad, personal.pasaporte from personal, tiene, buques, atraques, puertos where personal.genero = '$genero' and personal.cargo = '$cargo' and personal.pid = tiene.pid and buques.bid = tiene.bid and atraques.abid = buques.bid and puertos.ptid = atraques.puerto and UPPER(puertos.pnombre) like UPPER('%$puerto%');";
  
   #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
 	$result = $db -> prepare($query);
@@ -29,17 +29,18 @@
 <table class="table table-striped">
     <tr>
       <th>ID</th>
-      <th>Nombre</th>
-      <th>PATENTE</th>
-      <th>PAÍS</th>
-      <th>GIRO</th>
-      <th>N° PERSONAL</th>
+      <th>NOMBRE</th>
+      <th>EDAD</th>
+      <th>CARGO</th>
+      <th>GÉNERO</th>
+      <th>NACIONALIDAD</th>
+      <th>PASAPORTE</th>
     </tr>
   
       <?php
         // echo $pokemones;
         foreach ($buques as $buque) {
-          echo "<tr><td>$buque[0]</td><td>$buque[1]</td><td>$buque[2]</td><td>$buque[3]</td><td>$buque[4]</td><td>$buque[5]</td></tr>";
+          echo "<tr><td>$buque[0]</td><td>$buque[1]</td><td>$buque[2]</td><td>$buque[3]</td><td>$buque[4]</td><td>$buque[5]</td><td>$buque[6]</td></tr>";
       }
       ?>
       
