7d2c348f7de12be0059071672e61f9139c55de6e
Macarena Varela <you@example.com>
2020-09-27 15:00:33 -0300

    editando index

--- Sites/index.php
+++ Sites/index.php
@@ -3,7 +3,7 @@
 <body>
 <div class="container-fluid" style="position: relative; opacity: 40%;">
   
-  <div class="container-fluid middle center" style="background-color: white; opacity: 70%; position: absolute; margin-top: 20%; padding: 10%;">
+  <div class="container-fluid middle center" style="background-color: white; opacity: 100%; position: absolute; margin-top: 10%; padding: 5%;">
     
       <h1 align="center" ><b>B U Q U E P E D I A</b></h1>
       <p align="center">En esta página podras encontrar toda la información que desees respecto a los buques registrados en la ACMB.</p>
