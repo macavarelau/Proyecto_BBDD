6cdfa5abebdf8b7c7c03ce1f18340867867efd16
Macarena Varela <you@example.com>
2020-10-22 17:07:56 -0300

    fibonacci funcion

--- fibonacci.sql
+++ fibonacci.sql
@@ -11,7 +11,8 @@ SELECT INTO ret fib
 FROM
 fib_cache
 WHERE num = numero;
-IF ret IS NULL THENret := fib_cacheando(numero - 2) + fib_cacheando(numero - 1);
+IF ret IS NULL THEN
+ret := fib_cacheando(numero - 2) + fib_cacheando(numero - 1);
 INSERT INTO fib_cache (num, fib)
 VALUES (numero, ret);
 END IF;
