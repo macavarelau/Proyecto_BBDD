e247f42484ed28b34f9037b4b2830d582643ae0c
Macarena Varela <you@example.com>
2020-10-23 16:33:50 -0300

    crud

--- Sites/home.php
+++ Sites/home.php
@@ -234,7 +234,8 @@
     if($user_password==$user_cpassword)
     {
 
-        $query = "INSERT INTO usuarios VALUES (1, '%$user_name%', %$user_age%, '%$user_gender%', '%$user_passport%', '%$user_nationality%', '%$user_password%');";
+        $query = "INSERT INTO usuarios(user_id, nombre_usuario, edad, sexo, pasaporte, nacionalidad, contraseña)
+        VALUES (1, '%$user_name%', %$user_age%, '%$user_gender%', '%$user_passport%', '%$user_nationality%', '%$user_password%');";
         $result = $db -> prepare($query);
         $result -> execute();
         $usuarios = $result -> fetchAll();
@@ -252,5 +253,5 @@
         {
         echo '<script type="text/javascript"> alert("Confirmación de contraseña no coincide")</script>';
         }
-    }
+  }
   ?>
