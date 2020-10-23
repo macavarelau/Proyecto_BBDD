6bbc13cd4389407a6ce4dc48f9877121e45eb84e
Macarena Varela <you@example.com>
2020-10-23 16:20:38 -0300

    crud

--- Sites/home.php
+++ Sites/home.php
@@ -219,7 +219,7 @@
 
 <?php
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-  require("../config/conexion.php");
+  require("config/conexion.php");
 
   if(isset($_POST['login']))
   {
