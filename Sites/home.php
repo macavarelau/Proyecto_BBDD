964b2ff969410291da656fd56d023d8f8925a47f
Macarena Varela <you@example.com>
2020-10-23 13:29:35 -0300

    crud

--- Sites/home.php
+++ Sites/home.php
@@ -8,16 +8,28 @@
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
     <table class='table table-hover table-responsive table-bordered'>
         <tr>
-            <td>Name</td>
+            <td>Nombre</td>
             <td><input type='text' name='name' class='form-control' /></td>
         </tr>
         <tr>
-            <td>Description</td>
-            <td><textarea name='description' class='form-control'></textarea></td>
+            <td>Edad</td>
+            <td><input type='text' name='name' class='form-control' /></td>
         </tr>
         <tr>
-            <td>Price</td>
-            <td><input type='text' name='price' class='form-control' /></td>
+            <td>Sexo</td>
+            <td><input type='text' name='name' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td>Número de pasaporte</td>
+            <td><input type='text' name='name' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td>Contraseña</td>
+            <td><input type='text' name='name' class='form-control' /></td>
+        </tr>
+        <tr>
+            <td>Nacionalidad</td>
+            <td><input type='text' name='name' class='form-control' /></td>
         </tr>
         <tr>
             <td></td>
