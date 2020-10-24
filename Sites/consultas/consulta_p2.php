662fa0c10b9aed486313561a1f3cf70e68d0c805
Lucas <lzalaquett@uc.cl>
2020-10-24 19:50:34 -0300

    consultra de prueba 2

--- Sites/consultas/consulta_p2.php
+++ Sites/consultas/consulta_p2.php
@@ -6,15 +6,16 @@
 <?php
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
+  require("../config/data.php");
 
- 	$query = "SELECT DISTINCT pid, nombre FROM dblink('host=localhost user=grupo85 dbname=grupo85e3 password=pieza312 port=5432', 'SELECT rut as pid, nombre FROM personal') UNION SELECT DISTINCT pid, nombre FROM personal;";
+  $query = "SELECT DISTINCT pid, nombre FROM personal;";
 	$result = $db36 -> prepare($query);
 	$result -> execute();
-	$navieras = $result -> fetchAll();
+	$personas = $result -> fetchAll();
   ?>
   
     <br/><br/>
-	<h2 class="font-weight-bolder" >Aquí podrá ver lista con los nombres de todas las navieras:</h2>
+	<h2 class="font-weight-bolder" >Aquí podrá ver lista con los nombres de todas las $personas:</h2>
     <br/><br/>
   <table class="table table-striped table-hover">
     
@@ -23,8 +24,8 @@
       <th>NOMBRE</th>
     </tr>
   <?php
-	foreach ($navieras as $naviera) {
-      echo "<tr> <td>$naviera[0]</td> <td>$naviera[1]</td> </tr>";
+	foreach ($personas as $persona) {
+      echo "<tr> <td>$persona[0]</td> <td>$persona[1]</td> </tr>";
     }
       ?>
 </table>
