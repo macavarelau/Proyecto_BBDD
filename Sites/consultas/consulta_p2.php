197cee5222169b131e81a042c435eb559f4cc535
Lucas <lzalaquett@uc.cl>
2020-10-24 20:03:19 -0300

    consultra de prueba 2

--- Sites/consultas/consulta_p2.php
+++ Sites/consultas/consulta_p2.php
@@ -8,26 +8,24 @@
   require("../config/conexion.php");
   require("../config/data.php");
 
- 	$query = "SELECT DISTINCT * FROM public.dblink('host=$localhost user=$user2 dbname=$databaseName2 password=$password2 port=5432', 'SELECT * FROM personal');";
-	$result = $db36 -> prepare($query);
+ 	$query = "SELECT DISTINCT * FROM public.dblink('host=$localhost user=$user dbname=$databaseName password=$password port=5432', 'SELECT pid, nombre FROM personal');";
+	$result = $db85 -> prepare($query);
 	$result -> execute();
 	$personas = $result -> fetchAll();
   ?>
   
     <br/><br/>
-	<h2 class="font-weight-bolder" >Aquí podrá ver lista con los nombres de todas las $personas:</h2>
+	<h2 class="font-weight-bolder" >Aquí podrá ver lista con los nombres de todas las personas:</h2>
     <br/><br/>
   <table class="table table-striped table-hover">
     
     <tr>
       <th>ID</th>
       <th>NOMBRE</th>
-      <th>edad</th>
-      <th>sexo</th>
     </tr>
   <?php
 	foreach ($personas as $persona) {
-      echo "<tr> <td>$persona[0]</td> <td>$persona[1]</td> <td>$persona[2]</td> <td>$persona[3]</td> </tr>";
+      echo "<tr> <td>$persona[0]</td> <td>$persona[1]</td> </tr>";
     }
       ?>
 </table>
