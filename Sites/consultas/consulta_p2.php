f26dc3770405a9b7c901fa7749f410b9a9d0ad39
Lucas <lzalaquett@uc.cl>
2020-10-24 19:58:44 -0300

    consultra de prueba 2

--- Sites/consultas/consulta_p2.php
+++ Sites/consultas/consulta_p2.php
@@ -8,8 +8,8 @@
   require("../config/conexion.php");
   require("../config/data.php");
 
- 	$query = "SELECT DISTINCT rut, nombre FROM personal;";
-	$result = $db85 -> prepare($query);
+ 	$query = "SELECT DISTINCT * FROM public.dblink('host=localhost user=$user2 dbname=$databaseName2 password=$password2 port=5432', 'SELECT * FROM personal');";
+	$result = $db36 -> prepare($query);
 	$result -> execute();
 	$personas = $result -> fetchAll();
   ?>
@@ -22,10 +22,12 @@
     <tr>
       <th>ID</th>
       <th>NOMBRE</th>
+      <th>edad</th>
+      <th>sexo</th>
     </tr>
   <?php
 	foreach ($personas as $persona) {
-      echo "<tr> <td>$persona[0]</td> <td>$persona[1]</td> </tr>";
+      echo "<tr> <td>$persona[0]</td> <td>$persona[1]</td> <td>$persona[2]</td> <td>$persona[3]</td> </tr>";
     }
       ?>
 </table>
