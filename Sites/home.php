be4a5b26d89d5d1941c4ad008cea2cc837d5cfd9
Macarena Varela <you@example.com>
2020-10-25 19:37:00 -0300

    crud

--- Sites/home.php
+++ Sites/home.php
@@ -236,7 +236,7 @@
 
         $query = "INSERT INTO usuarios (nombre_usuario, edad, sexo, pasaporte, nacionalidad, contrasena)
         VALUES ('$user_name', $user_age, '$user_gender', '$user_passport', '$user_nationality', '$user_password');";
-        $result = $db -> prepare($query);
+        $result = $db36 -> prepare($query);
         $result -> execute();
         $usuarios = $result -> fetchAll();
 
