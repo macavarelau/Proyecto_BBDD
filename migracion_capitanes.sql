503bbb67c313e89dc635bee472ba7ed87a1ea32d
Macarena Varela <you@example.com>
2020-10-25 14:22:21 -0300

    migracion

--- migracion_capitanes.sql
+++ migracion_capitanes.sql
@@ -2,11 +2,8 @@ CREATE or REPLACE FUNCTION
 migrar_usuarios ()
 RETURNS void AS $$
 BEGIN
-  cont = 1
   FOR row in SELECT nombre, edad, genero, pasaporte, nacionalidad FROM personal WHERE cargo='capitán' LOOP
-    contrasena = CONCAT(SELECT LEFT(nombre,3), SELECT LEFT(pasaporte,3)
-    INSERT INTO usuarios VALUES (cont, row, contrasena)
-    cont = cont + 1
+    INSERT INTO usuarios VALUES (row, CONCAT(SELECT LEFT(nombre,3), SELECT LEFT(pasaporte,3))
   END LOOP;
 END
 $$ language plpgsql 
