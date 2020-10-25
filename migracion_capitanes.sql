aeafc3e692e9344e5d345e20c4a6fe1971a25871
Macarena Varela <you@example.com>
2020-10-25 15:34:53 -0300

    migracion

--- migracion_capitanes.sql
+++ migracion_capitanes.sql
@@ -6,7 +6,7 @@ DECLARE
   t_row personal%rowtype;
 BEGIN
   FOR t_row in t_curs LOOP
-    INSERT INTO usuarios(nombre, edad, sexo, pasaporte, nacionalidad, contrasena) VALUES (t_row.nombre, t_row.edad, t_row.genero, t_row.pasaporte, t_row.nacionalidad, CONCAT(LEFT(t_row.nombre, 3), LEFT(t_row.pasaporte, 3));
+    INSERT INTO usuarios(nombre, edad, sexo, pasaporte, nacionalidad, contrasena) VALUES (t_row.nombre, t_row.edad, t_row.genero, t_row.pasaporte, t_row.nacionalidad, CONCAT(LEFT(t_row.nombre, 3), LEFT(t_row.pasaporte, 3)));
   END LOOP;
 END;
 $$ language plpgsql;
