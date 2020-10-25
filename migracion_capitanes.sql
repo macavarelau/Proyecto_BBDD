a2cbe4301033d24f249952926691ff595aaeb8b2
Macarena Varela <you@example.com>
2020-10-25 14:42:49 -0300

    migracion

--- migracion_capitanes.sql
+++ migracion_capitanes.sql
@@ -2,7 +2,7 @@ CREATE or REPLACE FUNCTION
 migrar_usuarios ()
 RETURNS void AS $$
 DECLARE 
-  t_curs cursor for SELECT nombre, edad, genero, pasaporte, nacionalidad FROM personal WHERE cargo='capitán'
+  t_curs cursor for SELECT nombre, edad, genero, pasaporte, nacionalidad FROM personal WHERE cargo='capitán';
   t_row personal%rowtype;
 BEGIN
   FOR t_row in t_curs LOOP
