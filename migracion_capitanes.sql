03a1544d2e0c19cd3a1a1dfeb7db5d6500d90161
Macarena Varela <you@example.com>
2020-10-25 14:31:26 -0300

    migracion

--- migracion_capitanes.sql
+++ migracion_capitanes.sql
@@ -4,7 +4,7 @@ RETURNS void AS $$
 DECLARE row personal%rowtype;
 BEGIN
   FOR row in SELECT nombre, edad, genero, pasaporte, nacionalidad FROM personal WHERE cargo='capitán' LOOP
-    INSERT INTO usuarios VALUES (row, '123456')
+    INSERT INTO usuarios VALUES (row, '123456');
   END LOOP;
 END
 $$ language plpgsql 
