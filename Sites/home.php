066baf0c1b1d0011849ea38bc8afc3bc8617511e
Macarena Varela <you@example.com>
2020-10-26 16:00:46 -0300

    login

--- Sites/home.php
+++ Sites/home.php
@@ -17,5 +17,16 @@
   </li>
 </ul>
 
+<ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Ver mi perfil</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="navieras/index.php">Navieras</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="puertos/index.php">Puertos</a>
+  </li>
+</ul> 
 </body>
 </html>
