f8563c1b4cee78f251d1b803b36696bd6f4ee1a7
Macarena Varela <you@example.com>
2020-10-26 16:03:17 -0300

    login

--- Sites/home.php
+++ Sites/home.php
@@ -5,17 +5,6 @@
 <body>
 
 <!-- Nav tabs -->
-<ul class="nav nav-tabs">
-  <li class="nav-item">
-    <a class="nav-link active" data-toggle="tab" href="#perfil">Ver mi perfil</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" data-toggle="tab" href="navieras/index.php">Navieras</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" data-toggle="tab" href="puertos/index.php">Puertos</a>
-  </li>
-</ul>
 
 <ul class="nav nav-pills">
   <li class="nav-item">
--- Sites/navieras/show.php
+++ Sites/navieras/show.php
@@ -3,11 +3,11 @@
 <body>
 <?php
   require("../config/conexion.php");
-  $nid = $_POST("nid")
-  $nid = intval($nid)
+  $nid = $_POST("nid");
+  $nid = intval($nid);
 
   $query = "SELECT DISTINCT buques.bid, buques.nombre, buques.patente, buques.pais, buques.giro, buques.personal FROM navieras, posee, buques WHERE navieras.nid = $nid AND buques.bid = posee.bid AND navieras.nid = posee.nid ORDER BY buques.giro;";
-  $result = $db36 -> prepare($query)
+  $result = $db36 -> prepare($query);
   $result -> execute();
   $buques = $result -> fetchAll();
 ?>
@@ -19,7 +19,7 @@
     <th>Patente</th>
     <th>Pais</th>
     <th>Giro</th>
-    <th># Personal</th>
+    <th>Cantidad personal</th>
   </tr>
 
   <?php
