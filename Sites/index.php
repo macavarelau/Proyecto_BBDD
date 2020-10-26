b47576c4f7d277be859416dda7ac8b312d2e81e6
Macarena Varela <you@example.com>
2020-10-26 14:08:05 -0300

    login

--- Sites/index.php
+++ Sites/index.php
@@ -48,34 +48,20 @@ echo '
     $user_password = $_POST["password"];
 
     $query = "SELECT * FROM usuarios WHERE pasaporte='$user_passport';";
-    #$result = $db36 -> query($query);
     $result = $db36 -> query($query);
-    $result -> execute();
-    #$rows = pg_num_rows($result);
     $result -> execute();
     $usuarios = $result -> fetchAll();
     if (!$usuarios[0]){
-        echo "no existe";
+        echo '<script type="text/javascript"> alert("Usted no está registrado.")</script>';
     }
     if ($usuarios[0]){
-        echo "todo gucci";
-    }
-  #
-  #  if($rows == 0) {
-  #      echo "\nPDO::errorInfo():\n";
-  #      print_r($db36->errorInfo());
-  #      echo $rows . '<script type="text/javascript"> alert("Usted no está registrado.")</script>';
-  #  }     
-  #  else {
-  #      $result -> execute();
-  #      $usuarios = $result -> fetchAll();
-  #      $query_2 = "SELECT * FROM usuarios WHERE pasaporte='$user_passport' AND contrasena='$user_password';";
-  #      $result_2 = $db36 -> query($query);
-  #      if($result_2){
-  #         $result_2 -> execute();
-  #         $usuarios = $result_2 -> fetchAll();
-  #         echo $rows . '<script type="text/javascript"> alert("¡Bienvenido/a!")</script>';
-  #      }
-  #  }         
+        $query_2 = "SELECT * FROM usuarios WHERE pasaporte='$user_passport' AND contrasena='$user_password';";
+        $result_2 = $db36 -> query($query_2);
+        $result_2 -> execute();
+        $usuarios_2 = $result_2 -> fetchAll();
+        if($usuarios_2[6] == $user_password){
+            echo '<script type="text/javascript"> alert("¡Bienvenido/a!")</script>';
+         }
+     }         
   }
   ?>
