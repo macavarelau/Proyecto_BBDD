10f35dd3224d3c53723ca4b84a2bc85f470c7d8a
SamWeinstein <samuel.weinstein@uc.cl>
2020-10-26 17:58:44 -0300

    get passport en perfil

--- Sites/perfil/perfil.php
+++ Sites/perfil/perfil.php
@@ -1,5 +1,11 @@
-<?php include('../templates/header.html'); ?>
+<?php include('../templates/header.html');
+  require("../index.php");
+  echo "<h1>$user_passport</h1>";
+?>
 
+
+#E99654323
+#CriE99
 
 </div>
 </body>
