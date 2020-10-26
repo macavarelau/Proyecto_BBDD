27478ddcf157699cb88b8d1c41034ce0393c11da
Lucas <lzalaquett@uc.cl>
2020-10-26 19:19:28 -0300

    error de perfil

--- Sites/perfil/perfil.php
+++ Sites/perfil/perfil.php
@@ -4,18 +4,17 @@
   $user_passport = $_SESSION['user_passport'];
 
   $query = "SELECT nombre, edad, sexo, pasaporte, nacionalidad from usuarios where pasaporte = $user_passport;";
-  $result = $db36 -> prepare($query);
+  $result = $db36 -> query($query);
   $result -> execute();
   $user = $result -> fetchAll();
 
   echo "\nPDO::errorInfo():\n";
   print_r($db36->errorInfo());
-  
+
   foreach ($user as $u) {
       echo "<h1>$u[0]</h1><h2>$u[1]</h2><h1>$u[2]</h1><h2>$u[3]</h2><h1>$u[4]</h1>";
   }
 ?>
-Fatal error: Uncaught Error: Call to a member function prepare() on null in /home/grupo36/Sites/perfil/perfil.php:7 Stack trace: #0 {main} thrown in /home/grupo36/Sites/perfil/perfil.php on line 7
 </div>
 </body>
 
