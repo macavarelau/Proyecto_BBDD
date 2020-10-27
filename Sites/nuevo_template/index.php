40eb13a890c4fa53e9eeb74ad7f8e6fba517c0eb
Lucas <lzalaquett@uc.cl>
2020-10-27 01:44:12 -0300

    titulo

--- Sites/nuevo_template/index.php
+++ Sites/nuevo_template/index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Digital Agency</title>
-    <meta name="description" content="A Digital Agency Website landing page template built by TemplateFlip.com"/>
+    <title>Buquepedia Puertopedia</title>
+    <meta name="description" content="Una aplicación web para saber toda la info importante de los bos buques, navieras y puertos."/>
     <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
     <link href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" rel="stylesheet">
     <link href="css/aos.css" rel="stylesheet">
