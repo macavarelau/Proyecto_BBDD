f8216df671f0c9daac2476d0f06160618b1e9c70
benja <you@example.combyguiloff@uc.cl>
2020-10-27 02:46:40 -0300

    eliminar day

--- capacidad_instalacion.sql
+++ capacidad_instalacion.sql
@@ -4,7 +4,7 @@ RETURNS TABLE (instalacion_id text, fecha timestamp) AS $$
 DECLARE 
   contador INT;
   t_curs cursor for SELECT t.day::date FROM generate_series(f_inicio, f_termino, interval '1 day') AS t(day);
-  t_row t(day)%rowtype;
+  t_row t%rowtype;
   t_curs2 cursor for (SELECT permisos.permiso_id as pid, permisos.atraque as fecha, permisos.instalacion_id as iid, instalaciones.capacidad as cap FROM permisos, instalaciones, puertos WHERE permisos.instalacion_id = instalaciones.instalacion_id AND instalaciones.puerto_id = puertos.puerto_id AND puertos.puerto_id = idpuerto) as permisos_puerto(pid int, fecha date, iid int), cap int;
   t_row2 permisos_puerto%rowtype;
 BEGIN
