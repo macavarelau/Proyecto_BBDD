bbc5c932f5903c4269edea71402f3ee2758a22d1
benja <you@example.combyguiloff@uc.cl>
2020-10-27 02:45:15 -0300

    cambio de orden

--- capacidad_instalacion.sql
+++ capacidad_instalacion.sql
@@ -1,9 +1,9 @@
 CREATE or REPLACE FUNCTION
-capacidad_instalacion (timestamp f_inicio, timestamp f_termino, int idpuerto )
-RETURNS TABLE (text instalacion_id, timestamp fecha) AS $$
+capacidad_instalacion (f_inicio timestamp, f_termino timestamp, idpuerto int)
+RETURNS TABLE (instalacion_id text, fecha timestamp) AS $$
 DECLARE 
   contador INT;
-  t_curs cursor for SELECT t.day::date FROM generate_series(timestamp f_inicio, timestamp f_termino, interval '1 day') AS t(day);
+  t_curs cursor for SELECT t.day::date FROM generate_series(f_inicio, f_termino, interval '1 day') AS t(day);
   t_row t(day)%rowtype;
   t_curs2 cursor for (SELECT permisos.permiso_id as pid, permisos.atraque as fecha, permisos.instalacion_id as iid, instalaciones.capacidad as cap FROM permisos, instalaciones, puertos WHERE permisos.instalacion_id = instalaciones.instalacion_id AND instalaciones.puerto_id = puertos.puerto_id AND puertos.puerto_id = idpuerto) as permisos_puerto(pid int, fecha date, iid int), cap int;
   t_row2 permisos_puerto%rowtype;
