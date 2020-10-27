6e172eaccfba737dc70eea470e98510c29f38a2d
Macarena Varela <you@example.com>
2020-10-27 02:17:56 -0300

    calculo capacidad inst.

---
+++ capacidad_instalacion.sql
@@ -0,0 +1,25 @@
+CREATE or REPLACE FUNCTION
+capacidad_instalacion (f_inicio DATE, f_termino DATE, idpuerto INT)
+RETURNS TABLE (instalacion_id text, fecha date) AS $$
+DECLARE 
+  contador INT;
+  t_curs cursor for SELECT t.day::date FROM generate_series(timestamp f_inicio, timestamp f_termino, interval '1 day') AS t(day);
+  t_row t(day)%rowtype;
+  t_curs2 cursor for (SELECT permisos.permiso_id as pid, permisos.atraque as fecha, permisos.instalacion_id as iid, instalaciones.capacidad as cap FROM permisos, instalaciones, puertos WHERE permisos.instalacion_id = instalaciones.instalacion_id AND instalaciones.puerto_id = puertos.puerto_id AND puertos.puerto_id = idpuerto) as permisos_puerto(pid int, fecha date, iid int), cap int;
+  t_row2 permisos_puerto%rowtype;
+BEGIN
+  CREATE TABLE CANTIDAD(instalacion int, dia date);
+  FOR t_row in t_curs LOOP
+    contador := 0
+    FOR t_row2 in t_curs2 LOOP
+      IF t_row2.fecha = t_row.day THEN
+        contador := contador + 1
+      END IF
+      IF contador < t_row2.cap THEN
+        INSERT INTO CANTIDAD VALUES(t_row2.iid, t_row2.fecha)
+      END IF;
+      RETURN QUERY SELECT * FROM CANTIDAD;
+      DROP TABLE CANTIDAD;
+      RETURN;
+END;
+$$ language plpgsql;
