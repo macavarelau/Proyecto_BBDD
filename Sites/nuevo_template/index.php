4bf7d101ef248d58d8abe300c637afe99408a5a7
Macarena Varela <you@example.com>
2020-10-26 22:47:53 -0300

    template

--- Sites/nuevo_template/index.php
+++ Sites/nuevo_template/index.php
@@ -45,7 +45,7 @@
     <div class="h3 pb-3 text-center text-white" data-aos="fade-up">Logéate</div>
     <p class="px-5 pb-5 text-center text-white" data-aos="fade-up">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
     <div class="row" data-aos="flip-left">
-      <form class="px-5 pb-5 text-center text-whiteaction=" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+      <form aling="center" class="px-5 pb-5 text-center text-whiteaction=" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
         <table class='table table-hover table-bordered table-center'>
             <tr>
                 <td class="px-5 pb-5 text-center text-white">Número de pasaporte</td>
