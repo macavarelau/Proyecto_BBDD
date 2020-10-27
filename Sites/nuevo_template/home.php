f6389a4f0484a6c0f0b52af1ec6773e80273023b
Lucas <lzalaquett@uc.cl>
2020-10-27 15:15:10 -0300

    cambio de colores

--- Sites/nuevo_template/home.php
+++ Sites/nuevo_template/home.php
@@ -16,7 +16,7 @@
   <body id="top">
     <div class="page-content">
       <div>
-<div class="da-section da-work bg-secondary" id="learn">
+<div class="da-section da-work bg-primary" id="learn">
   <div class="container">
     <div class="h3 pb-3 text-center text-white" data-aos="fade-up">HOME</div>
     <p class="px-5 pb-5 text-center text-white" data-aos="fade-up">Selecciona el item que quieras revisar.</p>
@@ -24,7 +24,7 @@
       <div class="col-md-4">
         <div class="card mb-3" data-aos="flip-left">
           <div class="card-body mt-4 mb-1 text-center"><i class="pb-3 text-primary fas fa-briefcase fa-3x"></i>
-            <div class="h4 pb-3" href="perfil/perfil.php"><a class="btn btn-primary" href="perfil/perfil.php">Ver mi perfil</a></div>
+            <div class="h4 pb-3" href="perfil/perfil.php"><a class="btn btn-secondary" href="perfil/perfil.php">Ver mi perfil</a></div>
             <p>En esta sección podrás ver tu perfil y tu información de usuario. además si es que eres Jefe de alguna instalación o Capitán de buque, podras fer tu información asociada.</p>
           </div>
         </div>
