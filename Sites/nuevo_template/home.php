be0746c57576b96a4095546b7e768cd0d7381911
Lucas <lzalaquett@uc.cl>
2020-10-27 15:18:55 -0300

    colorrrrr

--- Sites/nuevo_template/home.php
+++ Sites/nuevo_template/home.php
@@ -23,8 +23,8 @@
     <div class="row">
       <div class="col-md-4">
         <div class="card mb-3" data-aos="flip-left">
-          <div class="card-body mt-4 mb-1 text-center"><i class="pb-3 text-secondary fas fa-briefcase fa-3x"></i>
-            <div class="h4 pb-3" href="perfil/perfil.php"><a class="btn btn-primary" href="perfil/perfil.php">Ver mi perfil</a></div>
+          <div class="card-body mt-4 mb-1 text-center"><i class="pb-3 text-primary fas fa-briefcase fa-3x"></i>
+            <div class="h4 pb-3" href="perfil/perfil.php"><a class="btn btn-gray" href="perfil/perfil.php">Ver mi perfil</a></div>
             <p>En esta sección podrás ver tu perfil y tu información de usuario. además si es que eres Jefe de alguna instalación o Capitán de buque, podras fer tu información asociada.</p>
           </div>
         </div>
