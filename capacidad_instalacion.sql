c57f82e00f4c0d51c9dbde2b480f3ab721a92bcb
benja <you@example.combyguiloff@uc.cl>
2020-10-27 11:22:04 -0300

    probando

--- capacidad_instalacion.sql
+++ capacidad_instalacion.sql
@@ -3,8 +3,8 @@ capacidad_instalacion (f_inicio timestamp, f_termino timestamp, idpuerto int)
 RETURNS TABLE (instalacion_id text, fecha_atraques timestamp) AS $$
 DECLARE 
   contador INT;
-  t_curs cursor for SELECT t.day::date FROM generate_series(f_inicio, f_termino, interval '1 day') AS t(day timestamp);
-  t_row t%rowtype;
+  t_curs cursor for SELECT t.day::date FROM generate_series(f_inicio, f_termino, interval '1 day') AS tab_fechas(day timestamp);
+  t_row tab_fechas%rowtype;
   t_curs2 cursor for (SELECT permisos.permiso_id as pid, permisos.atraque as fecha, permisos.instalacion_id as iid, 
                       instalaciones.capacidad as cap FROM permisos, instalaciones, puertos WHERE 
                       permisos.instalacion_id = instalaciones.instalacion_id AND instalaciones.puerto_id = puertos.puerto_id 
