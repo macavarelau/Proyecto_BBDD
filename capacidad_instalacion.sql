f9c342f422744cd66480be4d83b91afce869598a
benjaguiloff <48481681+benjaguiloff@users.noreply.github.com>
2020-10-27 19:02:42 -0300

    Update capacidad_instalacion.sql

--- capacidad_instalacion.sql
+++ capacidad_instalacion.sql
@@ -13,11 +13,11 @@ BEGIN
       IF rec2.puerto_id = idpuerto THEN
         contador := 0;
         FOR rec3 IN SELECT DISTINCT * FROM dblink('host=localhost user=grupo85 dbname=grupo85e3 password=pieza312 port=5432', 'SELECT permiso_id, atraque, permisos.instalacion_id FROM permisos') AS f2(permiso_id int, atraque date, instalacion_id int) LOOP
-          IF rec2.instalacion_id = rec3.instalacion_id AND DATE(rec3.atraque) = rec1.day AND rec2.puerto_id = idpuerto THEN
+          IF rec2.instalacion_id = rec3.instalacion_id AND DATE(rec3.atraque) = rec1.day THEN
             contador := contador + 1;
           END IF;
         END LOOP;
-        IF contador < rec2.capacidad AND rec2.puerto_id = idpuerto THEN
+        IF contador < rec2.capacidad THEN
           INSERT INTO T1 VALUES(rec2.instalacion_id, rec1.day, contador);
         END IF;
       END IF;
