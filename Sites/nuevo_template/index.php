680bce79902320f9c942cce4f4080345de757904
Macarena Varela <you@example.com>
2020-10-26 22:48:43 -0300

    template

--- Sites/nuevo_template/index.php
+++ Sites/nuevo_template/index.php
@@ -158,3 +158,37 @@
     <script src="scripts/main.js"></script>
   </body>
 </html>
+
+<?php
+  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  require("config/conexion.php");
+
+  if(isset($_POST['login']))
+  {
+    $user_passport = $_POST["user_passport"];
+    $user_password = $_POST["password"];
+    $_SESSION['user_passport'] = $user_passport;
+
+    $query = "SELECT * FROM usuarios WHERE pasaporte='$user_passport';";
+    $result = $db36 -> query($query);
+    $result -> execute();
+    $usuarios = $result -> fetchAll();
+    if (!$usuarios[0]){
+        echo '<script type="text/javascript"> alert("Usted no está registrado.")</script>';
+    }
+    if ($usuarios[0]){
+        $query_2 = "SELECT * FROM usuarios WHERE pasaporte='$user_passport' AND contrasena='$user_password';";
+        $result_2 = $db36 -> query($query_2);
+        $result_2 -> execute();
+        $usuarios_2 = $result_2 -> fetchAll();
+        if($usuarios_2[0][6] == $user_password){
+            echo '<script type="text/javascript"> alert("¡Bienvenido/a!")</script>';
+            echo '<script type="text/javascript">location.href = "home.php";</script>';
+            exit();
+         }
+        if($usuarios_2[0][6] != $user_password){
+            echo '<script type="text/javascript"> alert("Contraseña incorrecta. Intente nuevamente.")</script>';
+        }
+     }         
+  }
+?>
