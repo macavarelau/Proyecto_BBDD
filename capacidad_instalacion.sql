b94246266ff1c653c1020cb8562644e2b1875009
Macarena Varela <you@example.com>
2020-10-27 14:11:16 -0300

    consulta 1 puertos

--- capacidad_instalacion.sql
+++ capacidad_instalacion.sql
@@ -7,17 +7,17 @@ DECLARE
   t_data record;
   t_curs2 cursor for SELECT * FROM dblink('host=localhost user=grupo85 dbname=grupo85e3 password=pieza312 port=5432', 'SELECT permisos.permiso_id as pid, permisos.atraque as fecha, permisos.instalacion_id as iid, instalaciones.capacidad as cap FROM permisos, instalaciones, puertos WHERE permisos.instalacion_id = instalaciones.instalacion_id AND instalaciones.puerto_id = puertos.puerto_id 
   AND puertos.puerto_id = idpuerto') AS permisos_puerto(pid int, fecha timestamp, iid int, cap int);
-  t_row2 record;
+  t_data2 record;
 BEGIN
   CREATE TABLE CANTIDAD(instalacion int, dia timestamp);
   FOR t_data in t_curs LOOP
     contador := 0
-    FOR t_row2 in t_curs2 LOOP
-      IF t_row2.fecha = t_data.day THEN
+    FOR t_data2 in t_curs2 LOOP
+      IF t_data2.fecha = t_data.day THEN
         contador := contador + 1
       END IF
-      IF contador < t_row2.cap THEN
-        INSERT INTO CANTIDAD VALUES(t_row2.iid, t_row2.fecha)
+      IF contador < t_data2.cap THEN
+        INSERT INTO CANTIDAD VALUES(t_data2.iid, t_data2.fecha)
       END IF;
       RETURN QUERY SELECT * FROM CANTIDAD;
       DROP TABLE CANTIDAD;
