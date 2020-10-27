7fee5c57f432318ad902429a5fac1c0ee452235d
Lucas <lzalaquett@uc.cl>
2020-10-27 15:01:35 -0300

    añadimos ver perdil

--- Sites/nuevo_template/home.php
+++ Sites/nuevo_template/home.php
@@ -18,13 +18,13 @@
       <div>
 <div class="da-section da-work bg-secondary" id="learn">
   <div class="container">
-    <div class="h3 pb-3 text-center text-white" data-aos="fade-up">Logéate</div>
-    <p class="px-5 pb-5 text-center text-white" data-aos="fade-up">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+    <div class="h3 pb-3 text-center text-white" data-aos="fade-up">HOME</div>
+    <p class="px-5 pb-5 text-center text-white" data-aos="fade-up">Selecciona el item que quieras revisar.</p>
     <div class="row">
       <div class="col-md-4">
         <div class="card mb-3" data-aos="flip-left">
           <div class="card-body mt-4 mb-1 text-center"><i class="pb-3 text-primary fas fa-briefcase fa-3x"></i>
-            <div class="h4 pb-3">Experience</div>
+            <div class="h4 pb-3" href="perfil/perfil.php">Ver mi perfil</div>
             <p>Lorem ipsum dolor sit amet consectetur adipiscing elit primis rutrum, nullam tempor malesuada laoreet tempus blandit pretium etc.</p>
           </div>
         </div>
