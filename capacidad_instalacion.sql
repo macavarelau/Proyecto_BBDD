f21323d364eea67220c18b843535492cbcf3f0b6
benja <you@example.combyguiloff@uc.cl>
2020-10-27 11:17:09 -0300

    probando

--- capacidad_instalacion.sql
+++ capacidad_instalacion.sql
@@ -1,11 +1,14 @@
 CREATE or REPLACE FUNCTION
 capacidad_instalacion (f_inicio timestamp, f_termino timestamp, idpuerto int)
-RETURNS TABLE (instalacion_id text, fecha timestamp) AS $$
+RETURNS TABLE (instalacion_id text, fecha_atraques timestamp) AS $$
 DECLARE 
   contador INT;
   t_curs cursor for SELECT t.day::date FROM generate_series(f_inicio, f_termino, interval '1 day') AS t(day);
   t_row generate_series(f_inicio, f_termino, interval '1 day')%rowtype;
-  t_curs2 cursor for (SELECT permisos.permiso_id as pid, permisos.atraque as fecha, permisos.instalacion_id as iid, instalaciones.capacidad as cap FROM permisos, instalaciones, puertos WHERE permisos.instalacion_id = instalaciones.instalacion_id AND instalaciones.puerto_id = puertos.puerto_id AND puertos.puerto_id = idpuerto) as permisos_puerto(pid int, fecha date, iid int), cap int;
+  t_curs2 cursor for (SELECT permisos.permiso_id as pid, permisos.atraque as fecha, permisos.instalacion_id as iid, 
+                      instalaciones.capacidad as cap FROM permisos, instalaciones, puertos WHERE 
+                      permisos.instalacion_id = instalaciones.instalacion_id AND instalaciones.puerto_id = puertos.puerto_id 
+                      AND puertos.puerto_id = idpuerto) as permisos_puerto(pid int, fecha timestamp, iid int, cap int);
   t_row2 permisos_puerto%rowtype;
 BEGIN
   CREATE TABLE CANTIDAD(instalacion int, dia timestamp);
