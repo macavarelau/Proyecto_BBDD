7453467ead11e0dc5a7d2b1b0219db2fe591ffc6
benja <you@example.combyguiloff@uc.cl>
2020-10-27 02:24:04 -0300

    kncoin

--- capacidad_instalacion.sql
+++ capacidad_instalacion.sql
@@ -1,9 +1,9 @@
 CREATE or REPLACE FUNCTION
-capacidad_instalacion (f_inicio DATE, f_termino DATE, idpuerto INT)
+capacidad_instalacion (f_inicio timestamp, f_termino timestamp, idpuerto INT)
 RETURNS TABLE (instalacion_id text, fecha date) AS $$
 DECLARE 
   contador INT;
-  t_curs cursor for SELECT t.day::date FROM generate_series(timestamp f_inicio, timestamp f_termino, interval '1 day') AS t(day);
+  t_curs cursor for SELECT t.day::date FROM generate_series(f_inicio timestamp, f_termino timestamp, interval '1 day') AS t(day);
   t_row t(day)%rowtype;
   t_curs2 cursor for (SELECT permisos.permiso_id as pid, permisos.atraque as fecha, permisos.instalacion_id as iid, instalaciones.capacidad as cap FROM permisos, instalaciones, puertos WHERE permisos.instalacion_id = instalaciones.instalacion_id AND instalaciones.puerto_id = puertos.puerto_id AND puertos.puerto_id = idpuerto) as permisos_puerto(pid int, fecha date, iid int), cap int;
   t_row2 permisos_puerto%rowtype;
