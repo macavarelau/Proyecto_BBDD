6545e1fd643ffb7ecddc1ebfc4697d909698ecf4
benja <you@example.combyguiloff@uc.cl>
2020-10-27 12:13:46 -0300

    probando

--- capacidad_instalacion.sql
+++ capacidad_instalacion.sql
@@ -7,7 +7,7 @@ BEGIN
   FOR rec1 IN 
     SELECT t.day::date FROM generate_series(f_inicio, f_termino, interval '1 day') AS t(day)
   LOOP
-    RETURN NEXT rec.day;
+    RETURN NEXT rec1.day;
   END LOOP;
 END;
 $$ language plpgsql;
