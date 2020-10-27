6556f71e7dd5fc9692d291a5a5760bc78fcc5ac6
benjaguiloff <48481681+benjaguiloff@users.noreply.github.com>
2020-10-27 18:49:16 -0300

    Update capacidad_instalacion.sql

--- capacidad_instalacion.sql
+++ capacidad_instalacion.sql
@@ -9,10 +9,10 @@ DECLARE
 BEGIN
   CREATE TABLE T1(instalacion int, dia date, cant_barcos int);
   FOR rec1 IN SELECT t.day::date FROM generate_series(f_inicio, f_termino, interval '1 day') AS t(day) LOOP
-    FOR rec2 IN SELECT DISTINCT * FROM dblink('host=localhost user=grupo85 dbname=grupo85e3 password=pieza312 port=5432', 'SELECT instalaciones.instalacion_id, capacidad FROM instalaciones where puerto_id = idpuerto') AS f1(instalacion_id int, capacidad int) LOOP
+    FOR rec2 IN SELECT DISTINCT * FROM dblink('host=localhost user=grupo85 dbname=grupo85e3 password=pieza312 port=5432', 'SELECT instalaciones.instalacion_id, capacidad FROM instalaciones') AS f1(instalacion_id int, capacidad int) LOOP
       contador := 0;
       FOR rec3 IN SELECT DISTINCT * FROM dblink('host=localhost user=grupo85 dbname=grupo85e3 password=pieza312 port=5432', 'SELECT permiso_id, atraque, permisos.instalacion_id FROM permisos') AS f2(permiso_id int, atraque date, instalacion_id int) LOOP
-        IF rec2.instalacion_id = rec3.instalacion_id AND DATE(rec3.atraque) = rec1.day THEN
+        IF rec2.instalacion_id = rec3.instalacion_id AND DATE(rec3.atraque) = rec1.day AND rec2.puerto_id = idpuerto THEN
           contador := contador + 1;
         END IF;
       END LOOP;
